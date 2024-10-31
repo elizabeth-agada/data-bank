a199467a4a1d662e3c83efae064d4f70100be188
Vixs101 <elijahvix695@gmail.com>
2024-10-31 12:40:49 +0100

    Feature: Added the how-it-works page

--- projects/data-bank-frontend/src/components/Navbar.tsx
+++ projects/data-bank-frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -10,43 +10,45 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="grid grid-cols-12 max-w-7xl mx-auto items-center bg-[#2b2b2b] bg-opacity-50 rounded-2xl py-4 px-6 mt-5 text-white">
-
+      <div className="grid grid-cols-12 max-w-7xl mx-auto items-center bg-[#b5b5b5] bg-opacity-30 rounded-2xl py-4 px-6 mt-5 text-white">
         <div className="col-span-6 md:col-span-4 flex justify-start">
           <img src="/img/logo.png" alt="Logo" className="h-8" />
         </div>
 
-
-        <div className="hidden md:col-span-4 md:flex justify-center space-x-6 font-semibold text-lg">
-          <a href="#" className="hover:text-[#2B9DDA]">Home</a>
-          <a href="/how" className="hover:text-[#2B9DDA]">How Does It Work</a>
-          <a href="#" className="hover:text-[#2B9DDA]">About Us</a>
+        <div className="hidden md:col-span-4 md:flex justify-center md:space-x-2 lg:space-x-6 font-semibold text-lg">
+          <a href="/" className="hover:text-[#2B9DDA]">
+            Home
+          </a>
+          <a href="/how-it-works" className="hover:text-[#2B9DDA]">
+            How Does It Work
+          </a>
+          <a href="#" className="hover:text-[#2B9DDA]">
+            About Us
+          </a>
         </div>
 
         <div className="col-span-6 md:col-span-4 flex justify-end">
-          <button
-            className="md:px-5 txet-sm py-2 bg-white text-black rounded-2xl font-semibold hover:bg-gray-200 transition duration-200"
-          >
+          <button className="md:px-5 txet-sm py-2 bg-white text-black rounded-2xl font-semibold hover:bg-gray-200 transition duration-200">
             Connect Wallet
           </button>
 
-
           <div className="md:hidden ml-4" onClick={toggleMobileMenu}>
-            {isMobileMenuOpen ? (
-              <XMarkIcon className="h-8 w-8 text-white" />
-            ) : (
-              <Bars3Icon className="h-8 w-8 text-white" />
-            )}
+            {isMobileMenuOpen ? <XMarkIcon className="h-8 w-8 text-white" /> : <Bars3Icon className="h-8 w-8 text-white" />}
           </div>
         </div>
       </div>
 
-
       {isMobileMenuOpen && (
         <div className="flex flex-col items-center space-y-2 mt-3 md:hidden bg-[#2b2b2b] bg-opacity-50 rounded-lg py-3 text-white">
-          <a href="#" className="hover:text-[#2B9DDA] font-semibold text-lg">Home</a>
-          <a href="/how" className="hover:text-[#2B9DDA] font-semibold text-lg">How Does It Work</a>
-          <a href="#" className="hover:text-[#2B9DDA] font-semibold text-lg">About Us</a>
+          <a href="#" className="hover:text-[#2B9DDA] font-semibold text-lg">
+            Home
+          </a>
+          <a href="/how" className="hover:text-[#2B9DDA] font-semibold text-lg">
+            How Does It Work
+          </a>
+          <a href="#" className="hover:text-[#2B9DDA] font-semibold text-lg">
+            About Us
+          </a>
         </div>
       )}
     </>
